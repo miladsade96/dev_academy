c1aef9b5671938e1a65f0bc235723f8b9af77d0c
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-02-13 22:21:25 +0330

    Writing test functions to test countOccurrences functionality

---
+++ src/01-basic-challenges/main.test.js
@@ -0,0 +1,41 @@
+const functions = require("./main");
+const { countOccurrencesV1, countOccurrencesV2, countOccurrencesV3 } =
+  functions;
+
+describe("countOccurrences functions tests", () => {
+  it.each([
+    { totalString: "Hello", desiredChar: "l", expected: 2 },
+    { totalString: "Milad", desiredChar: "a", expected: 1 },
+    { totalString: "JavaScript", desiredChar: "s", expected: 0 },
+    { totalString: "Rebecca", desiredChar: "c", expected: 2 },
+  ])(
+    "countOccurrencesV1($totalString, $desiredChar) is equal to $expected",
+    ({ totalString, desiredChar, expected }) => {
+      expect(countOccurrencesV1(totalString, desiredChar)).toBe(expected);
+    },
+  );
+
+  it.each([
+    { totalString: "Hello", desiredChar: "l", expected: 2 },
+    { totalString: "Milad", desiredChar: "a", expected: 1 },
+    { totalString: "JavaScript", desiredChar: "s", expected: 0 },
+    { totalString: "Rebecca", desiredChar: "c", expected: 2 },
+  ])(
+    "countOccurrencesV2($totalString, $desiredChar) equals to $expected",
+    ({ totalString, desiredChar, expected }) => {
+      expect(countOccurrencesV2(totalString, desiredChar)).toBe(expected);
+    },
+  );
+
+  it.each([
+    { totalString: "Hello", desiredChar: "l", expected: 2 },
+    { totalString: "Milad", desiredChar: "a", expected: 1 },
+    { totalString: "JavaScript", desiredChar: "s", expected: 0 },
+    { totalString: "Rebecca", desiredChar: "c", expected: 2 },
+  ])(
+    "countOccurrencesV3($totalString, $desiredChar) equals to $expected",
+    ({ totalString, desiredChar, expected }) => {
+      expect(countOccurrencesV3(totalString, desiredChar)).toBe(expected);
+    },
+  );
+});
