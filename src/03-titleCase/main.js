c30991f1735ac8261662bf1cee114cb5eab60221
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-13 21:49:10 +0330

    Writing function signatures for titleCase challenge

---
+++ src/03-titleCase/main.js
@@ -0,0 +1,17 @@
+function titleCaseV1(str) {
+  // Solution #1 - Using for loop
+}
+
+function titleCaseV2(str) {
+  // Solution #2 - Using map method
+}
+
+function titleCaseV3(str) {
+  // Solution #3 - using regex
+}
+
+module.exports = {
+  titleCaseV1,
+  titleCaseV2,
+  titleCaseV3,
+};
