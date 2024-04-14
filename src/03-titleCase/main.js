621dd6759da01afde0880918df420d733c3e784c
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-14 23:12:33 +0330

    Implementing titleCase challenge solutions

--- src/03-titleCase/main.js
+++ src/03-titleCase/main.js
@@ -1,13 +1,38 @@
 function titleCaseV1(str) {
-  // Solution #1 - Using for loop
+  /* Solution #1 - Using for loop
+   *
+   * Time complexity: O(n)
+   * Space complexity: O(n)
+   */
+  const words = str.toLowerCase().split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  }
+  return words.join(" ");
 }
 
 function titleCaseV2(str) {
-  // Solution #2 - Using map method
+  /* Solution #2 - Using map method
+   *
+   * Time complexity: O(n)
+   * Space complexity: O(n) + O(n) = O(n)
+   */
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+    .join(" ");
 }
 
 function titleCaseV3(str) {
-  // Solution #3 - using regex
+  /* Solution #3 - using regex
+   *
+   * Time complexity: ?
+   * Space complexity: ?
+   * Time and space complexity of regex is hard to calculate due to internal implementation of regex.
+   * Please read this article for more details: https://swtch.com/~rsc/regexp/regexp1.html
+   */
+  return str.toLowerCase().replace(/\b\w/g, (match) => match.toUpperCase());
 }
 
 module.exports = {
