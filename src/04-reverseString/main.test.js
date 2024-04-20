773b419c9b2210221738be5abe9d560ef749668b
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-20 23:02:05 +0330

    Writing tests for reverseString Challenge

---
+++ src/04-reverseString/main.test.js
@@ -0,0 +1,24 @@
+const functions = require("./main");
+const { reverseStringV1, reverseStringV2 } = functions;
+
+describe("reverseString function tests", () => {
+  it.each([
+    { str: "", expected: "" },
+    { str: "M", expected: "M" },
+    { str: "Milad Sadeghi", expected: "ihgedaS daliM" },
+    { str: "JavaScript", expected: "tpircSavaJ" },
+    { str: "0123456789", expected: "9876543210" },
+  ])("reverseStringV1($str) equals to $expected", ({ str, expected }) => {
+    expect(reverseStringV1(str)).toBe(expected);
+  });
+
+  it.each([
+    { str: "", expected: "" },
+    { str: "S", expected: "S" },
+    { str: "TypeScript", expected: "tpircSepyT" },
+    { str: "Algorithms", expected: "smhtiroglA" },
+    { str: "Data Structures", expected: "serutcurtS ataD" },
+  ])("reverseStringV2($str) equals to $expected", ({ str, expected }) => {
+    expect(reverseStringV2(str)).toBe(expected);
+  });
+});
