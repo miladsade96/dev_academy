c9f82e92db66e1d9beae6c94edf3e586458c000a
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-21 22:07:01 +0330

    Implementing reverseString challenge solutions

--- src/04-reverseString/main.js
+++ src/04-reverseString/main.js
@@ -1,9 +1,23 @@
 function reverseStringV1(str) {
-  // Using for loop:
+  /* Solution #1 - Using for loop:
+   *
+   * Time complexity: O(n)
+   * Space complexity: O(n) - In general, 16bits per character
+   */
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
 }
 
 function reverseStringV2(str) {
-  // Using arrays:
+  /* Solution #2 - Using array:
+   *
+   * Time complexity: O(n)
+   * Space complexity: O(n)
+   */
+  return str.split("").reverse().join("");
 }
 
 module.exports = {
