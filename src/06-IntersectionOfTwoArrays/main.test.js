bbc190c68fcf43919eb0c0fda244a7e664c9175f
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-05-05 14:06:49 +0330

    Writing tests for intersection of two arrays

---
+++ src/06-IntersectionOfTwoArrays/main.test.js
@@ -0,0 +1,32 @@
+const functions = require("./main");
+const { intersectionV1, intersectionV2 } = functions;
+
+describe("intersectionOfTwoArrays function tests", () => {
+  it.each([
+    { arr1: [1, 2, 3, 4, 5], arr2: [2, 3, 4, 5, 6], expected: [2, 3, 4, 5] },
+    { arr1: [1, 2, 3], arr2: [2, 3, 4, 5, 6, 6, 7], expected: [2, 3] },
+    { arr1: [1, 2, 3, 4, 5, 6, 6, 7, 7, 8], arr2: [5, 5, 6], expected: [5, 6] },
+    { arr1: [], arr2: [2, 3, 4, 5, 6], expected: [] },
+    { arr1: [1, 2, 3, 4, 5], arr2: [], expected: [] },
+    { arr1: [1, 1, 1, 1, 1], arr2: [1, 1, 1, 1, 1], expected: [1] },
+  ])(
+    "intersectionV1($arr1, $arr2) equals to $expected",
+    ({ arr1, arr2, expected }) => {
+      expect(intersectionV1(arr1, arr2)).toStrictEqual(expected);
+    },
+  );
+
+  it.each([
+    { arr1: [1, 2, 3, 4, 5], arr2: [2, 3, 4, 5, 6], expected: [2, 3, 4, 5] },
+    { arr1: [1, 2, 3], arr2: [2, 3, 4, 5, 6, 6, 7], expected: [2, 3] },
+    { arr1: [1, 2, 3, 4, 5, 6, 6, 7, 7, 8], arr2: [5, 5, 6], expected: [5, 6] },
+    { arr1: [], arr2: [2, 3, 4, 5, 6], expected: [] },
+    { arr1: [1, 2, 3, 4, 5], arr2: [], expected: [] },
+    { arr1: [1, 1, 1, 1, 1], arr2: [1, 1, 1, 1, 1], expected: [1] },
+  ])(
+    "intersectionV2($arr1, $arr2) equals to $expected",
+    ({ arr1, arr2, expected }) => {
+      expect(intersectionV2(arr1, arr2)).toStrictEqual(expected);
+    },
+  );
+});
