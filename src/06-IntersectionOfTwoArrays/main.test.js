824818f3d06e64b34802253ef84b955ff638f2a8
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2024-05-06 16:18:17 +0330

    Writing tests for intersection v3

--- src/06-IntersectionOfTwoArrays/main.test.js
+++ src/06-IntersectionOfTwoArrays/main.test.js
@@ -1,5 +1,5 @@
 const functions = require("./main");
-const { intersectionV1, intersectionV2 } = functions;
+const { intersectionV1, intersectionV2, intersectionV3 } = functions;
 
 describe("intersectionOfTwoArrays function tests", () => {
   it.each([
@@ -27,6 +27,20 @@ describe("intersectionOfTwoArrays function tests", () => {
     "intersectionV2($arr1, $arr2) equals to $expected",
     ({ arr1, arr2, expected }) => {
       expect(intersectionV2(arr1, arr2)).toStrictEqual(expected);
+    },
+  );
+
+  it.each([
+    { arr1: [1, 2, 3, 4, 5], arr2: [2, 3, 4, 5, 6], expected: [2, 3, 4, 5] },
+    { arr1: [1, 2, 3], arr2: [2, 3, 4, 5, 6, 6, 7], expected: [2, 3] },
+    { arr1: [1, 2, 3, 4, 5, 6, 6, 7, 7, 8], arr2: [5, 5, 6], expected: [5, 6] },
+    { arr1: [], arr2: [2, 3, 4, 5, 6], expected: [] },
+    { arr1: [1, 2, 3, 4, 5], arr2: [], expected: [] },
+    { arr1: [1, 1, 1, 1, 1], arr2: [1, 1, 1, 1, 1], expected: [1] },
+  ])(
+    "intersectionV3($arr1, $arr2) equals to $expected",
+    ({ arr1, arr2, expected }) => {
+      expect(intersectionV3(arr1, arr2)).toStrictEqual(expected);
     },
   );
 });
