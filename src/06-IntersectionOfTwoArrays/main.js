b0d47a96a0d52127c92e07605225036399d7d9e4
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2024-05-07 22:13:19 +0330

    Adding time and space complexity comments for all implementations

--- src/06-IntersectionOfTwoArrays/main.js
+++ src/06-IntersectionOfTwoArrays/main.js
@@ -1,4 +1,11 @@
 function intersectionV1(arr1, arr2) {
+  /*
+   * Approach #1: Comparing two unique arrays
+   *
+   * Time Complexity: O(n * m) or O(n^2)
+   * Space Complexity; O(n)
+   */
+
   const uniqueArr1 = [...new Set(arr1)];
   const uniqueArr2 = [...new Set(arr2)];
   const intersectionArr = [];
@@ -15,12 +22,26 @@ function intersectionV1(arr1, arr2) {
 }
 
 function intersectionV2(arr1, arr2) {
+  /*
+   * Approach #2 - Solution #1: Comparing a unique array to a set
+   *
+   * Time Complexity: O(n * 1) or O(n)
+   * Space Complexity; O(n)
+   */
+
   const uniqueArr1 = [...new Set(arr1)];
   const set2 = new Set(arr2);
   return uniqueArr1.filter((el) => set2.has(el));
 }
 
 function intersectionV3(arr1, arr2) {
+  /*
+   * Approach #2 - Solution #2: Comparing two sets
+   *
+   * Time Complexity: O(n * 1) or O(n)
+   * Space Complexity; O(n)
+   */
+
   const set1 = new Set(arr1);
   const set2 = new Set(arr2);
   const intersectionArr = [];
