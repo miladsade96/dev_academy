583d8efb82ad99c4a8d98e72a6c246c20fd23d10
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-05-25 22:03:48 +0330

    Implementing tests for missing letter challenge

---
+++ src/08-missingLetter/main.test.js
@@ -0,0 +1,32 @@
+const functions = require("./main");
+const { findMissingLetterV1, findMissingLetterV2 } = functions;
+
+describe("findMissingLetter function tests", () => {
+  it.each([
+    { arrOfChars: [], expected: "" },
+    { arrOfChars: ["a"], expected: "" },
+    { arrOfChars: ["a", "c"], expected: "b" },
+    { arrOfChars: ["a", "b", "c"], expected: "" },
+    { arrOfChars: ["A", "B", "C", "E"], expected: "D" },
+    { arrOfChars: ["A", "B", "C", "D", "E", "F", "G"], expected: "" },
+  ])(
+    "findMissingLetterV1($arrOfChars) is equal to: $expected",
+    ({ arrOfChars, expected }) => {
+      expect(findMissingLetterV1(arrOfChars)).toBe(expected);
+    },
+  );
+
+  it.each([
+    { arrOfChars: [], expected: "" },
+    { arrOfChars: ["a"], expected: "" },
+    { arrOfChars: ["a", "c"], expected: "b" },
+    { arrOfChars: ["a", "b", "c"], expected: "" },
+    { arrOfChars: ["A", "B", "C", "E"], expected: "D" },
+    { arrOfChars: ["A", "B", "C", "D", "E", "F", "G"], expected: "" },
+  ])(
+    "findMissingLetterV2($arrOfChars) is equal to: $expected",
+    ({ arrOfChars, expected }) => {
+      expect(findMissingLetterV2(arrOfChars)).toBe(expected);
+    },
+  );
+});
