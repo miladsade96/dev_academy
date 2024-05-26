b00e475f96dc629e9f7b7d47161dc525bbbd1fb0
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-05-26 23:09:56 +0330

    Implementing missing letter solution - approach #1

--- src/08-missingLetter/main.js
+++ src/08-missingLetter/main.js
@@ -1,4 +1,20 @@
-function findMissingLetterV1(arrOfChars) {}
+function findMissingLetterV1(arrOfChars) {
+  /*
+   * Approach #1: Using a reference string
+   *
+   * Time Complexity: O(n)
+   * Space Complexity; O(1)
+   */
+
+  const alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const startIndex = alphabets.indexOf(arrOfChars.at(0));
+  for (let i = 1; i < arrOfChars.length; i++) {
+    if (arrOfChars.at(i) !== alphabets.at(startIndex + i)) {
+      return alphabets.at(startIndex + i);
+    }
+  }
+  return "";
+}
 
 function findMissingLetterV2(arrOfChars) {}
 
