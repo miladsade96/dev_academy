44ab10ea88744c269c3a1897d32b18153e771580
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-05-26 23:10:03 +0330

    Implementing missing letter solution - approach #2

--- src/08-missingLetter/main.js
+++ src/08-missingLetter/main.js
@@ -16,6 +16,24 @@ function findMissingLetterV1(arrOfChars) {
   return "";
 }
 
-function findMissingLetterV2(arrOfChars) {}
+function findMissingLetterV2(arrOfChars) {
+  /*
+   * Approach #1: Using character codes (UTF-16)
+   *
+   * Time Complexity: O(n)
+   * Space Complexity; O(1)
+   */
+
+  if (arrOfChars.length === 0) return "";
+  let startCode = arrOfChars.at(0).charCodeAt(0);
+  for (let i = 1; i < arrOfChars.length; i++) {
+    const currentCode = arrOfChars.at(i).charCodeAt(0);
+    if (currentCode - startCode > 1) {
+      return String.fromCharCode(startCode + 1);
+    }
+    startCode = currentCode;
+  }
+  return "";
+}
 
 module.exports = { findMissingLetterV1, findMissingLetterV2 };
