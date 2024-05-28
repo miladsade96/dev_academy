628c63b00e1a145d08725812df564f2b2e666741
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-05-28 23:43:46 +0330

    Writing test suits for third, fourth and fifth approach

--- src/08-missingLetter/main.test.js
+++ src/08-missingLetter/main.test.js
@@ -37,4 +37,49 @@ describe("findMissingLetter function tests", () => {
       expect(findMissingLetterV2(arrOfChars)).toBe(expected);
     },
   );
+
+  it.each([
+    { arrOfChars: [], expected: "" },
+    { arrOfChars: ["a"], expected: "" },
+    { arrOfChars: ["a", "c"], expected: "b" },
+    { arrOfChars: ["a", "b", "c"], expected: "" },
+    { arrOfChars: ["A", "B", "C", "E"], expected: "D" },
+    { arrOfChars: ["A", "B", "C", "D", "E", "F", "G"], expected: "" },
+    { arrOfChars: ["C", "D", "E", "G"], expected: "F" },
+  ])(
+    "findMissingLetterV3($arrOfChars) is equal to: $expected",
+    ({ arrOfChars, expected }) => {
+      expect(findMissingLetterV3(arrOfChars)).toBe(expected);
+    },
+  );
+
+  it.each([
+    { arrOfChars: [], expected: "" },
+    { arrOfChars: ["a"], expected: "" },
+    { arrOfChars: ["a", "c"], expected: "b" },
+    { arrOfChars: ["a", "b", "c"], expected: "" },
+    { arrOfChars: ["A", "B", "C", "E"], expected: "D" },
+    { arrOfChars: ["A", "B", "C", "D", "E", "F", "G"], expected: "" },
+    { arrOfChars: ["C", "D", "E", "G"], expected: "F" },
+  ])(
+    "findMissingLetterV4($arrOfChars) is equal to: $expected",
+    ({ arrOfChars, expected }) => {
+      expect(findMissingLetterV4(arrOfChars)).toBe(expected);
+    },
+  );
+
+  it.each([
+    { arrOfChars: [], expected: "" },
+    { arrOfChars: ["a"], expected: "" },
+    { arrOfChars: ["a", "c"], expected: "b" },
+    { arrOfChars: ["a", "b", "c"], expected: "" },
+    { arrOfChars: ["A", "B", "C", "E"], expected: "D" },
+    { arrOfChars: ["A", "B", "C", "D", "E", "F", "G"], expected: "" },
+    { arrOfChars: ["C", "D", "E", "G"], expected: "F" },
+  ])(
+    "findMissingLetterV5($arrOfChars) is equal to: $expected",
+    ({ arrOfChars, expected }) => {
+      expect(findMissingLetterV5(arrOfChars)).toBe(expected);
+    },
+  );
 });
