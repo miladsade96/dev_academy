6e6b4e24717f628a178bd6bb5c918f32b4358130
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-07-06 23:27:47 +0330

    Writing test suite for permutations challenge

---
+++ src/15-permutations/main.test.js
@@ -0,0 +1,11 @@
+import { describe, it, expect } from "vitest";
+import { permutations } from "./main.js";
+
+describe("permutations", () => {
+  it.each([
+    { str: "abc", result: ["abc", "acb", "bac", "bca", "cab", "cba"] },
+    { str: "dog", result: ["dog", "dgo", "odg", "ogd", "gdo", "god"] },
+  ])("it should return $result for permutations($str)", ({ str, result }) => {
+    expect(permutations(str)).toEqual(result);
+  });
+});
