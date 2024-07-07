89318f27d79662ec95aaff3d92aa0208de129717
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-07-07 22:27:58 +0330

    Adding a new test case for flattenArray test suite

--- src/14-flattenArray/main.test.js
+++ src/14-flattenArray/main.test.js
@@ -8,6 +8,7 @@ describe("flattenArray", () => {
     { arr: [1, [2, [3]]], result: [1, 2, 3] },
     { arr: [1, [2, [3, [4]]]], result: [1, 2, 3, 4] },
     { arr: [1, [2, [3, [4, [5]]]]], result: [1, 2, 3, 4, 5] },
+    { arr: [[[[[[[[[[5]]]]]]]]]], result: [5] },
   ])("it should return $result for flattenArray($arr)", ({ arr, result }) => {
     expect(flattenArray(arr)).toEqual(result);
   });
