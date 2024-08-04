be8b45f6727534c9a681abf4a7db30d81dfd96bd
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-08-04 22:22:21 +0330

    Writing test suits for permutations challenge

--- src/15-permutations/main.test.js
+++ src/15-permutations/main.test.js
@@ -1,10 +1,39 @@
 import { describe, it, expect } from "vitest";
 import { permutations } from "./main.js";
 
-describe.skip("permutations", () => {
+describe("permutations", () => {
   it.each([
     { str: "abc", result: ["abc", "acb", "bac", "bca", "cab", "cba"] },
     { str: "dog", result: ["dog", "dgo", "odg", "ogd", "gdo", "god"] },
+    {
+      str: "abcd",
+      result: [
+        "abcd",
+        "abdc",
+        "acbd",
+        "acdb",
+        "adbc",
+        "adcb",
+        "bacd",
+        "badc",
+        "bcad",
+        "bcda",
+        "bdac",
+        "bdca",
+        "cabd",
+        "cadb",
+        "cbad",
+        "cbda",
+        "cdab",
+        "cdba",
+        "dabc",
+        "dacb",
+        "dbac",
+        "dbca",
+        "dcab",
+        "dcba",
+      ],
+    },
   ])("it should return $result for permutations($str)", ({ str, result }) => {
     expect(permutations(str)).toEqual(result);
   });
