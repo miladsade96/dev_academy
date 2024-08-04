f9272ba0d08d64b236d3849311d4a01e28011f6f
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-08-04 22:21:57 +0330

    Implementing the solution for permutations challenge

--- src/15-permutations/main.js
+++ src/15-permutations/main.js
@@ -1,1 +1,35 @@
-export function permutations() {}
+export function permutations(str) {
+  /*
+   * Approach: Using recursion and backtracking
+   *
+   * Time complexity: O(n * n!) where:
+   *
+   *    n: number of recursive calls
+   *    n!: number of calculated permutations
+   *
+   * Space complexity: O(n * n!) where:
+   *
+   *    n: number of recursive frames in the call stack
+   *    n!: size of the final result array
+   */
+  const result = [];
+
+  if (str.length === 2) {
+    result.push(str, `${str[1]}${str[0]}`);
+    return result;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const firstLetter = str[i];
+
+    const restOfString = str.slice(0, i) + str.slice(i + 1);
+
+    const subPermutations = permutations(restOfString);
+
+    for (let j = 0; j < subPermutations.length; j++) {
+      result.push(firstLetter + subPermutations[j]);
+    }
+  }
+
+  return result;
+}
