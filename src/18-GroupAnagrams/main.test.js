dccb2be0069450370ddbd043e62d3dc511a9e596
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-09-08 19:35:47 +0330

    Writing test suite for groupAnagramV1 and groupAnagramV2 functions

--- src/18-GroupAnagrams/main.test.js
+++ src/18-GroupAnagrams/main.test.js
@@ -5,3 +5,36 @@ function transformer(arr) {
   return arr.map((innerArr) => innerArr.join(""));
 }
 
+describe("GroupAnagrams", () => {
+  it.each([
+    { strs: [""], result: [[""]] },
+    { strs: ["a"], result: [["a"]] },
+    {
+      strs: ["eat", "tea", "ate", "nat", "tan", "bat"],
+      result: [["eat", "tea", "ate"], ["nat", "tan"], ["bat"]],
+    },
+  ])(
+    "It should return $result for groupAnagramsV1($strs)",
+    ({ strs, result }) => {
+      const transformedGroupAnagramsV1 = transformer(groupAnagramsV1(strs));
+      const transformedResult = transformer(result);
+      expect(transformedGroupAnagramsV1).toMatchObject(transformedResult);
+    },
+  );
+
+  it.each([
+    { strs: [""], result: [[""]] },
+    { strs: ["a"], result: [["a"]] },
+    {
+      strs: ["eat", "tea", "ate", "nat", "tan", "bat"],
+      result: [["eat", "tea", "ate"], ["nat", "tan"], ["bat"]],
+    },
+  ])(
+    "It should return $result for groupAnagramsV2($strs)",
+    ({ strs, result }) => {
+      const transformedGroupAnagramsV1 = transformer(groupAnagramsV2(strs));
+      const transformedResult = transformer(result);
+      expect(transformedGroupAnagramsV1).toMatchObject(transformedResult);
+    },
+  );
+});
